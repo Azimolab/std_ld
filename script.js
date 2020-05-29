e9bdfcc32c81f6fe43430ae99c842107311b7b57
George Lai <george.lai@azimotech.com.br>
2020-05-29 07:28:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,40 +1,69 @@
+"use strict";
+
+var _this = void 0;
+
+function _newArrowCheck(innerThis, boundThis) { if (innerThis !== boundThis) { throw new TypeError("Cannot instantiate an arrow function"); } }
+
 // Design by Catalin V. (@hiskio https://twitter.com/hiskio) 
 // https://dribbble.com/shots/3408986-Info-Card-Daily-UI-045
+var slideElements = ['.back__slide', '.card__slide', '.content__slide'];
+var inProgress = false;
 
-const slideElements = ['.back__slide', '.card__slide', '.content__slide'];
-let inProgress = false;
+var goToSlide = function goToSlide(slideElements, index) {
+  var _this2 = this;
 
-const goToSlide = (slideElements, index) => {
+  _newArrowCheck(this, _this);
+
   if (!inProgress) {
     inProgress = true;
-
     $('.active').addClass('exit');
     $('.active').removeClass('active');
-    slideElements.forEach(elem => {
-      $(`${elem}:nth-child(${index})`).addClass('active');
-    });
+    slideElements.forEach(function (elem) {
+      _newArrowCheck(this, _this2);
 
-    const evenSlide = index % 2 === 0;
-    if (evenSlide)
-    $('.content__ping').addClass('content__ping--right');else
-
-    $('.content__ping').removeClass('content__ping--right');
+      $("".concat(elem, ":nth-child(").concat(index, ")")).addClass('active');
+    }.bind(this));
+    var evenSlide = index % 2 === 0;
+    if (evenSlide) $('.content__ping').addClass('content__ping--right');else $('.content__ping').removeClass('content__ping--right');
     $('.content__ping--noanimation').removeClass('content__ping--noanimation');
+    setTimeout(function () {
+      _newArrowCheck(this, _this2);
 
-    setTimeout(() => $('.exit').removeClass('exit'), 500);
-    setTimeout(() => inProgress = false, 500);
+      return $('.exit').removeClass('exit');
+    }.bind(this), 500);
+    setTimeout(function () {
+      _newArrowCheck(this, _this2);
+
+      return inProgress = false;
+    }.bind(this), 500);
   }
-};
+}.bind(void 0);
 
-$('.content__slide:nth-child(1) .button').on('click', () => goToSlide(slideElements, 2));
-$('.content__slide:nth-child(2) .button').on('click', () => goToSlide(slideElements, 1));
+$('.content__slide:nth-child(1) .button').on('click', function () {
+  _newArrowCheck(this, _this);
 
-setTimeout(() => goToSlide(slideElements, 2), 10);
-setTimeout(() => goToSlide(slideElements, 1), 1000);
+  return goToSlide(slideElements, 2);
+}.bind(void 0));
+$('.content__slide:nth-child(2) .button').on('click', function () {
+  _newArrowCheck(this, _this);
+
+  return goToSlide(slideElements, 1);
+}.bind(void 0));
+setTimeout(function () {
+  _newArrowCheck(this, _this);
+
+  return goToSlide(slideElements, 2);
+}.bind(void 0), 10);
+setTimeout(function () {
+  _newArrowCheck(this, _this);
+
+  return goToSlide(slideElements, 1);
+}.bind(void 0), 1000); 
+
+
 
 // let amount = 0;
 // let slide = 0;
-
 // const progress = () => {
 //   amount++
 //   $('.active .progress').css('transform', `scaleX(${amount/400})`)
@@ -50,5 +79,4 @@ setTimeout(() => goToSlide(slideElements, 1), 1000);
 //     }, 2000);
 //   }
 // }
-
 // let progressInterval = setInterval(progress,20);
